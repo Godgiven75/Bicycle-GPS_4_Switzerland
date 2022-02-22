5248df60234d482ad3dfb97271904b899018271b
GodGiven75 <tanguy.dieudonne02@gmail.com>
2022-02-22 14:25:09 +0100

    Merge remote-tracking branch 'origin/main'

--- ch/epfl/javelo/projection/SwissBounds.java
+++ ch/epfl/javelo/projection/SwissBounds.java
@@ -1,5 +1,14 @@
 package epfl.javelo.projection;
 
 public final class SwissBounds {
-    double MIN_E = 2_485_
+    public final double MIN_E = 2_485_000;
+    public final double MAX_E = 2_834_000;
+    public final double MIN_N = 1_075_000;
+    public final double MAX_N = 1_296_000;
+    public final double WIDTH = MAX_E - MIN_E;
+    public final double HEIGHT = MAX_N - MIN_N;
+
+    public static boolean containsEN(double e, double n) {
+        return ((MIN_E < e)&&(e < MAX_E) && (MIN_N < n)&&(n < MAX_N));
+    }
 }
