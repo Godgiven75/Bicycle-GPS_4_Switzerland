689584d2104a1f6069a38c4fb16becc16f40f4a2
GodGiven75 <tanguy.dieudonne02@gmail.com>
2022-02-22 14:50:33 +0100

    Merge remote-tracking branch 'origin/main'

--- ch/epfl/javelo/projection/Ch1903.java
+++ ch/epfl/javelo/projection/Ch1903.java
@@ -3,5 +3,24 @@ package epfl.javelo.projection;
 public final class Ch1903 {
     private Ch1903() {}
 
+    //longitude lambda, latitude phi
 
+    /**
+     *
+     * @param lon
+     * @param lat
+     * @return the coordinate E of the point originally in WGS 84 coordinates
+     */
+    public static double e(double lon, double lat) {
+        double l1 = Math.pow(10,-4)*(3600*Math.toDegrees(lon) - 26_782.5);
+        double phi1 = Math.pow(10,-4)*(3600*Math.toDegrees(lat) - 169_028.66);
+
+        return 2_600_072.37 + 211_455.93*l1 - 10_938.51*l1*phi1 - 0.36*l1*phi1*phi1 - 44.54*Math.pow(l1,3);
+    }
+
+    public static double n(double lon, double lat) {
+        double l1 = Math.pow(10,-4)*(3600*Math.toDegrees(lon) - 26_782.5);
+        double phi1 = Math.pow(10,-4)*(3600*Math.toDegrees(lat) - 169_028.66);
+
+    }
 }
--- ch/epfl/javelo/projection/SwissBounds.java
+++ ch/epfl/javelo/projection/SwissBounds.java
@@ -3,12 +3,13 @@ package epfl.javelo.projection;
 public final class SwissBounds {
     private SwissBounds() {}
 
-    public final double MIN_E = 2_485_000;
-    public final double MAX_E = 2_834_000;
-    public final double MIN_N = 1_075_000;
-    public final double MAX_N = 1_296_000;
-    public final double WIDTH = MAX_E - MIN_E;
-    public final double HEIGHT = MAX_N - MIN_N;
+    //demander si static ok
+    public final static double MIN_E = 2_485_000;
+    public final static double MAX_E = 2_834_000;
+    public final static double MIN_N = 1_075_000;
+    public final static double MAX_N = 1_296_000;
+    public final static double WIDTH = MAX_E - MIN_E;
+    public final static double HEIGHT = MAX_N - MIN_N;
 
     public static boolean containsEN(double e, double n) {
         return ((MIN_E < e)&&(e < MAX_E) && (MIN_N < n)&&(n < MAX_N));
