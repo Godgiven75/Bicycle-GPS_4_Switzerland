31368524519154d7c6682cce4a31f6c3f20c3d9b
GodGiven75 <tanguy.dieudonne02@gmail.com>
2022-02-25 12:19:45 +0100

    Merge remote-tracking branch 'origin/main'

--- ch/epfl/javelo/projection/data/AttributeSet.java
+++ ch/epfl/javelo/projection/data/AttributeSet.java
@@ -1,4 +1,15 @@
 package epfl.javelo.projection.data;
 
-public record AttributeSet() {
+public record AttributeSet(long bits) {
+    public AttributeSet {
+        String stringBits = Long.toString(bits);
+        int length = (int) (Math.log10(bits)+1);
+        if (length > Attribute.values().length) {
+            for (int i=Attribute.values().length; i < length; i++) {
+                if (stringBits.charAt(i) == 1) {
+                    throw new IllegalArgumentException();
+                }
+            }
+        }
+    }
 }
