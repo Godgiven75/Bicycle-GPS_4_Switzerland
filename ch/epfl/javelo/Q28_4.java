3165bf6760f2ab41abe4d3181b887b2bf97f42ea
Ngirod <nathanael.girod@epfl.ch>
2022-02-28 16:02:18 +0100

    tests bits

--- ch/epfl/javelo/Q28_4.java
+++ ch/epfl/javelo/Q28_4.java
@@ -1,22 +1,37 @@
 package epfl.javelo;
 
-
+/**
+ * Classe finale et non-instanciable permettant de convertir des nombres entre la représentation Q28_4 et d'autres représentations
+ */
 public final class Q28_4 {
     private Q28_4() {}
 
-    public static void main(String[] args) {
-        System.out.println(ofInt(0b10011100 ));
-    }
-    // scalb et l'opérateur de décalage revoient une interprétation non-signée, ce qui doit être compensée en soustreyant
+    // scalb et l'opérateur de décalage renvoient une interprétation non-signée, ce qui doit être compensé en soustreyant
     // 16. Y a-t-il une meilleure façon de faire cela ?
+
+    /**
+     * Retourne la valeur Q28.4 correspondant à l'entier donné
+     * @param i entier
+     * @return a valeur Q28.4 correspondant à l'entier donné
+     */
     public static int ofInt(int i){
         return (i >>> 4) - 16;
     }
 
+    /**
+     * Retourne la valeur de type double égale à la valeur Q28.4 donnée
+     * @param q28_4 valeur donnée
+     * @return la valeur de type double égale à la valeur Q28.4 donnée
+     */
     public static double asDouble(int q28_4) {
         return Math.scalb(q28_4, -4) - 16 ;
     }
 
+    /**
+     * Retourne la valeur de type float égale à la valeur Q28.4 donnée
+     * @param q28_4 valeur donnée
+     * @return la valeur de type float égale à la valeur Q28.4 donnée
+     */
     public static float asFloat(int q28_4) {
         return Math.scalb(q28_4, -4) - 16 ;
     }
--- ch/epfl/javelo/projection/PointWebMercator.java
+++ ch/epfl/javelo/projection/PointWebMercator.java
@@ -1,21 +1,44 @@
 package epfl.javelo.projection;
 
+import epfl.javelo.Preconditions;
+
+/**
+ * Enregistrement représentant un point dans le système Web Mervator
+ * Ses attributs sont :  double x, la coordonnée x du point; double y la coordonée y du point.
+ */
 public record PointWebMercator(double x, double y) {
 
     private static final int BASE_ZOOM_LEVEL = 8;
 
+    /**
+     * Constructeur validant les coordonnées x et y du point et levant une expressions si l'une d'elle n'est pas comprise dans l'intervalle [0;1]
+     * @param x coordonnée x du point
+     * @param y coordonnée y du point
+     */
     public PointWebMercator {
-        if( !(0 <= x && x <= 1) || !(0 <= y && y <= 1)) throw new IllegalArgumentException();
+        Preconditions.checkArgument( !(0 <= x && x <= 1) || !(0 <= y && y <= 1));
     }
 
+    /**
+     * Retourne le point dont les coordonnées x et y sont au niveau de zoom zoomLevel
+     * @param zoomLevel niveau de zoom
+     * @param x coordonnée x du point au niveau de zoom
+     * @param y coordonnée y du point au niveau de zoom
+     * @return le point dont les coordonnées x et y sont au niveau de zoom zoomLevel
+     */
     public static PointWebMercator of(int zoomLevel, double x, double y ) {
-        if(!(zoomLevel >= 0 && zoomLevel <= 20)) throw new IllegalArgumentException();
+        Preconditions.checkArgument(!(zoomLevel >= 0 && zoomLevel <= 20));
 
         double scaledX = Math.scalb(x, - (zoomLevel + BASE_ZOOM_LEVEL));
         double scaledY = Math.scalb(y, - (zoomLevel + BASE_ZOOM_LEVEL));
         return new PointWebMercator(scaledX, scaledY);
     }
 
+    /**
+     * Retourne le point WebMercator correspondant au point du système de coordonnées suisse donné
+     * @param pointCh point du système de coordonnées suisse
+     * @return le point WebMercator correspondant au point du système de coordonnées suisse donné
+     */
     public static PointWebMercator ofPointCh(PointCh pointCh) {
         double longitude = pointCh.lon();
         double latitude = pointCh.lat();
@@ -26,25 +49,48 @@ public record PointWebMercator(double x, double y) {
         return new PointWebMercator(x, y);
     }
 
+    /**
+     * Retourne la coordonnée x du point au niveau de zoom donné
+     * @param zoomLevel niveau de zoom
+     * @return Retourne la coordonnée x du point au niveau de zoom donné
+     */
     public double xAtZoomLevel(int zoomLevel) {
         if(!(zoomLevel >= 0 && zoomLevel <= 20)) throw new IllegalArgumentException();
         return Math.scalb(x, zoomLevel + BASE_ZOOM_LEVEL );
     }
 
+    /**
+     * Retourne la coordonnée y du point au niveau de zoom donné
+     * @param zoomLevel niveau de zoom
+     * @return Retourne la coordonnée x du point au niveau de zoom donné
+     */
     public double yAtZoomLevel(int zoomLevel) {
         if(!(zoomLevel >= 0 && zoomLevel <= 20)) throw new IllegalArgumentException();
         return Math.scalb(y, zoomLevel + BASE_ZOOM_LEVEL );
 
     }
 
+    /**
+     * Retourne la longitude du point, en radians
+     * @return la longitude du point, en radians
+     */
     public double lon() {
         return WebMercator.lon(x);
     }
 
+    /**
+     * Retourne la latitude du point, en radians
+     * @return la latitude du point, en radians
+     */
     public double lat() {
         return WebMercator.lat(y);
     }
 
+    /**
+     * Retourne le point de coordonnées suisses se trouvant à la même position que le récepteur (this) ou null si ce point n'est pas dans les limites de la Suisse définies par SwissBounds
+     * @return le point de coordonnées suisses se trouvant à la même position que le récepteur (this) ou null si ce point n'est pas dans les limites de la Suisse définies par SwissBounds
+     *
+     */
     public PointCh toPointCh() {
         double lon = lon();
         double lat = lat();
