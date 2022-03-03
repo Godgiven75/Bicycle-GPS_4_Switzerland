2afc14fdf8a8c0aa851525ea8dadd6437f732a6c
GodGiven75 <tanguy.dieudonne02@gmail.com>
2022-03-04 00:20:33 +0100

    Etape 3

--- src/ch/epfl/javelo/Bits.java
+++ src/ch/epfl/javelo/Bits.java
@@ -15,7 +15,7 @@ public final class Bits {
      */
     public static int extractSigned (int value, int start, int length) {
         int rangeSize = start + length;
-        Preconditions.checkArgument(0 <= start && length >= 1 && rangeSize <= 32 ); //demander pour la précondition
+        Preconditions.checkArgument(0 <= start && rangeSize <= 32 ); //demander pour la précondition
         int leftShift = value << 32 - rangeSize;
         return leftShift >> 32 - length;
     }
@@ -29,7 +29,7 @@ public final class Bits {
      */
     public static int extractUnsigned(int value, int start, int length) {
         int rangeSize = start + length;
-        Preconditions.checkArgument(0 <= start && length >= 1 && rangeSize <= 32 );
+        Preconditions.checkArgument(0 <= start && rangeSize < 32 );
         int leftShift = value <<  32 - rangeSize;
         return leftShift >>> 32 - length;
     }
