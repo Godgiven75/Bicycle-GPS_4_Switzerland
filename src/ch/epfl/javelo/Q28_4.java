1dea18b180a344b976aa938888e7bb6a426c288c
GodGiven75 <tanguy.dieudonne02@gmail.com>
2022-03-03 19:23:44 +0100

    Etape 3

--- src/ch/epfl/javelo/Q28_4.java
+++ src/ch/epfl/javelo/Q28_4.java
@@ -20,7 +20,6 @@ public final class Q28_4 {
      * @param q28_4 valeur donnée
      * @return la valeur de type double égale à la valeur Q28.4 donnée
      */
-
     public static double asDouble(int q28_4) {
         return Math.scalb(q28_4, -4);
     }
