045b7bd12d2506fcbd001cb9702834bc14f16f57
GodGiven75 <tanguy.dieudonne02@gmail.com>
2022-03-14 11:10:40 +0100

    Etape 3

---
+++ src/ch/epfl/javelo/data/Graph.java
@@ -0,0 +1,21 @@
+package ch.epfl.javelo.data;
+
+import java.io.IOException;
+
+public class Graph {
+
+    /**
+     * Retourne le graphe JaVelo obtenu à partir des fichiers se trouvant dans le répertoire dont le chemin d'accès est
+     * basePath, ou lève IOException en cas d'erreur d'entrée/sortie, p. ex. si l'un des fichiers attendu n'existe pas.
+     * @param basePath
+     * @return retourne le graphe JaVelo obtenu à partir des fichiers se trouvant dans le répertoire dont le chemin
+     * d'accès est basePath, ou lève IOException en cas d'erreur d'entrée/sortie, p. ex. si l'un des fichiers attendu
+     * n'existe pas.
+     * @throws IOException
+     */
+    Graph loadFrom(Path basePath) throws IOException {
+
+    }
+
+
+}
