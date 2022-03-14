bf440549ece731a37a0ec990ae6aafe7c7438db1
GodGiven75 <tanguy.dieudonne02@gmail.com>
2022-03-14 15:13:49 +0100

    Etape 4

--- src/ch/epfl/javelo/data/Graph.java
+++ src/ch/epfl/javelo/data/Graph.java
@@ -1,8 +1,31 @@
 package ch.epfl.javelo.data;
 
+import ch.epfl.javelo.projection.PointCh;
+
 import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
 
 public class Graph {
+    public final GraphNodes nodes;
+    public final GraphSectors sectors;
+    public final GraphEdges edges;
+    public final List<AttributeSet> attributeSets;
+
+
+    /**
+     * Retourne le graphe avec les noeuds, secteurs, arêtes et ensemble d'attributs donnés
+     * @param nodes
+     * @param sectors
+     * @param edges
+     * @param attributeSets
+     */
+    public Graph (GraphNodes nodes, GraphSectors sectors, GraphEdges edges, List<AttributeSet> attributeSets) {
+        this.nodes = nodes;
+        this.sectors = sectors;
+        this.edges = edges;
+        this.attributeSets = attributeSets;
+    }
 
     /**
      * Retourne le graphe JaVelo obtenu à partir des fichiers se trouvant dans le répertoire dont le chemin d'accès est
@@ -17,5 +40,21 @@ public class Graph {
 
     }
 
+    /**
+     * Retourne le nombre total de noeuds dans le graphe
+     * @return le nombre total de noeuds dans le graphe
+     */
+    public int nodeCount() {
+        return nodes.count();
+    }
+
+    /**
+     * Retourne la position du noeud d'identité donnée
+     * @param nodeId
+     * @return la position du noeud d'identité donnée
+     */
+    public PointCh nodePoint(int nodeId) {
+        return
+    }
 
 }
