9c5df1f453257f5b7bced376a6bf1a3e027109fa
GodGiven75 <tanguy.dieudonne02@gmail.com>
2022-03-15 00:03:15 +0100

    Etape 4

--- src/ch/epfl/javelo/routing/Route.java
+++ src/ch/epfl/javelo/routing/Route.java
@@ -5,14 +5,58 @@ import ch.epfl.javelo.projection.PointCh;
 
 import java.util.List;
 
+/**
+ * Représente un itinéraire
+ */
 public interface Route {
 
+    /**
+     * Retourne l'index du segment à la position donnée (en mètres)
+     * @param position
+     * @return l'index du segment à la position donnée (en mètres)
+     */
     int indexOfSegmentAt(double position);
+
+    /**
+     * Retourne la longueur de l'itinéraire (en mètres)
+     * @return la longueur de l'itinéraire (en mètres)
+     */
     double length();
+
+    /**
+     * Retourne la totalité des arêtes de l'itinéraire
+     * @return la totalité des arêtes de l'itinéraire
+     */
     List<Edge> edges();
+
+    /**
+     * Retourne le point se trouvant à la position donnée le long de l'itinéraire
+     * @return le point se trouvant à la position donnée le long de l'itinéraire
+     */
     List<PointCh> points();
+
+    /**
+     * Retourne l'altitude à la position donnée le long de l'itinéraire
+     * @param position
+     * @return l'altitude à la position donnée le long de l'itinéraire
+     */
     PointCh pointAt(double position);
+
+    /**
+     * Retourne l'identité du noeud appartenant à l'itinéraire et se trouvant le plus proche de la position donnée
+     * @return l'identité du noeud appartenant à l'itinéaire et se trouvant le plus proche de la position donnée
+     */
     double elevationAt();
+
+    /**
+     * Retourne l'identité du noeud appartenant à l'itinéraire et se trouvant le plus proche de la position donnée
+     * @return l'identité du noeud appartenant à l'itinéraire et se trouvant le plus proche de la position donnée
+     */
     int nodeClosestTo();
+
+    /**
+     * Retourne le point de l'itinéraire se trouvant le plus proche du point de référence donné
+     * @return le point de l'itinéraire se trouvant le plus proche du point de référence donné
+     */
     RoutePoint pointClosestTo();
 }
