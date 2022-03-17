01e9e818098fc8b1eefb693b1398cea5bc119d17
GodGiven75 <tanguy.dieudonne02@gmail.com>
2022-03-18 00:25:15 +0100

    Etape 4

--- src/ch/epfl/javelo/data/GraphSectors.java
+++ src/ch/epfl/javelo/data/GraphSectors.java
@@ -13,10 +13,13 @@ import static java.lang.Short.toUnsignedInt;
  * Représente le tableau contenant les 16384 secteurs de Javelo
  */
 public record GraphSectors(ByteBuffer buffer) {
-    private static final double SECTOR_WIDTH = 349_000/128.0;
-    private static final double SECTOR_LENGTH = 221_000/128.0;
+    private static final double SWISS_WIDTH = 349_000;
+    private static final double SWISS_HEIGHT = 221_000;
     private static final double SWISS_E_MIN = 2_485_000;
     private static final double SWISS_N_MIN = 1_075_000;
+    private static final int SUBDIVISIONS_PER_SIDE = 128;
+    private static final double SECTOR_WIDTH = SWISS_WIDTH / SUBDIVISIONS_PER_SIDE;
+    private static final double SECTOR_HEIGHT = SWISS_HEIGHT / SUBDIVISIONS_PER_SIDE;
     private static final int OFFSET_SECTOR = Integer.BYTES + Short.BYTES;
 
     /**
@@ -35,16 +38,16 @@ public record GraphSectors(ByteBuffer buffer) {
         double distanceToWestLimit = leftSide - SWISS_E_MIN;
         double distanceToSouthLimit = lowerSide - SWISS_N_MIN;
 
-        int xMin = Math2.clamp(0, (int) (distanceToWestLimit / SECTOR_WIDTH), 128);
-        int xMax = Math2.clamp(0, (int) ( (distanceToWestLimit + 2 * distance) / SECTOR_WIDTH), 128);
-        int yMin = Math2.clamp(0, (int) (distanceToSouthLimit / SECTOR_LENGTH), 128);
-        int yMax = Math2.clamp(0, (int) ( (distanceToSouthLimit + 2 * distance) / SECTOR_LENGTH), 128);
+        int xMin = Math2.clamp(0, (int) (distanceToWestLimit / SECTOR_WIDTH), SUBDIVISIONS_PER_SIDE);
+        int xMax = Math2.clamp(0, (int) ( (distanceToWestLimit + 2 * distance) / SECTOR_WIDTH), SUBDIVISIONS_PER_SIDE);
+        int yMin = Math2.clamp(0, (int) (distanceToSouthLimit / SECTOR_HEIGHT), SUBDIVISIONS_PER_SIDE);
+        int yMax = Math2.clamp(0, (int) ( (distanceToSouthLimit + 2 * distance) / SECTOR_HEIGHT), SUBDIVISIONS_PER_SIDE);
 
         for (int x = xMin; x <= xMax; x++) {
             for (int y = yMin; y <= yMax; y++) {
-                int startNodeId = buffer.getInt(OFFSET_SECTOR * (x + y * 128));
+                int startNodeId = buffer.getInt(OFFSET_SECTOR * (x + y * SUBDIVISIONS_PER_SIDE));
                 int endNodeId = startNodeId
-                                + toUnsignedInt(buffer.getShort(OFFSET_SECTOR * (x + y * 128) + Integer.BYTES));
+                                + toUnsignedInt(buffer.getShort(OFFSET_SECTOR * (x + y * SUBDIVISIONS_PER_SIDE) + Integer.BYTES));
 
                 inArea.add(new Sector(startNodeId, endNodeId));
             }
