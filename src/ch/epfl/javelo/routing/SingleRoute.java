65c58c7d2303b4e38238d767e1f11601ede826fd
GodGiven75 <tanguy.dieudonne02@gmail.com>
2022-03-18 13:37:05 +0100

    Etape 4

---
+++ src/ch/epfl/javelo/routing/SingleRoute.java
@@ -0,0 +1,26 @@
+package ch.epfl.javelo.routing;
+
+import ch.epfl.javelo.Preconditions;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Représente un itinéraire simple, càd reliant un point de départ à un point d'arrivée, sans point de passage
+ * intermédiaire
+ */
+public final class SingleRoute {
+    private final List<Edge> edges;
+
+    /**
+     * Retourne l'itinéraire simple composé des arêtes données, ou lève IllegalArgumentException si la liste d'arêtes
+     * est vide
+     * @param edges
+     */
+    public SingleRoute(List<Edge> edges) {
+        Preconditions.checkArgument();
+        this.edges = Collections.unmodifiableList(edges);
+    }
+
+
+}
