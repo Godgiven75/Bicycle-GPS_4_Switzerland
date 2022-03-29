3bc95c14df7cb1f5a0eca047fc2a38075b17ba56
GodGiven75 <tanguy.dieudonne02@gmail.com>
2022-03-29 21:16:34 +0200

    Etape 6

--- src/ch/epfl/javelo/routing/RouteComputer.java
+++ src/ch/epfl/javelo/routing/RouteComputer.java
@@ -15,19 +15,22 @@ public class RouteComputer {
     }
 
     /**
-     * Retourne l'itinéraire de coût total minimal allant du noeud d'identité startNodeId au noeud d'identité endNodeId
-     * dans le graphe passé au constructeur, ou null si aucun itinéraire n'existe. Si le noeud de départ et d'arrivée
-     * sont identiques, lève IllegalArgumentException
+     * Retourne l'itinéraire de coût total minimal allant du noeud d'identité
+     * startNodeId au noeud d'identité endNodeId dans le graphe passé au
+     * constructeur, ou null si aucun itinéraire n'existe. Si le noeud de départ
+     * et d'arrivée sont identiques, lève IllegalArgumentException
      * @param startNodeId
      * @param endNodeId
-     * @return l'itinéraire de coût total minimal allant du noeud d'identité startNodeId au noeud d'identité endNodeId
-     * dans le graphe passé au constructeur, ou null si aucun itinéraire n'existe. Si le noeud de départ et d'arrivée
-     * sont identiques, lève IllegalArgumentException
+     * @return l'itinéraire de coût total minimal allant du noeud d'identité
+     * startNodeId au noeud d'identité endNodeId dans le graphe passé au
+     * constructeur, ou null si aucun itinéraire n'existe. Si le noeud de départ
+     * et d'arrivée sont identiques, lève IllegalArgumentException
      */
     public Route bestRouteBetween(int startNodeId, int endNodeId) {
         Preconditions.checkArgument(startNodeId != endNodeId);
 
-        record WeightedNode(int nodeId, float distance) implements Comparable<WeightedNode> {
+        record WeightedNode(int nodeId, float distance)
+                implements Comparable<WeightedNode> {
             @Override
             public int compareTo(WeightedNode that) {
                 return Float.compare(this.distance, that.distance);
@@ -36,40 +39,62 @@ public class RouteComputer {
 
         int nodesNb = graph.nodeCount();
         float[] distance = new float[nodesNb];
-        List<Edge> edges = new ArrayList<>();
+        int[] predecessor = new int[nodesNb];
+        List<Edge> edges = new ArrayList<>(nodesNb);
+        Deque<Edge> pathEdges = new LinkedList<>();
         PriorityQueue<WeightedNode> en_exploration = new PriorityQueue<>();
         CityBikeCF cityBikeCF = new CityBikeCF(graph);
         WeightedNode closestNode = new WeightedNode(0, 0);
 
         Arrays.fill(distance, Float.POSITIVE_INFINITY);
+        Arrays.fill(predecessor, 0);
         distance[startNodeId] = 0f;
         en_exploration.add(new WeightedNode(startNodeId, 0));
         float dist = 0f;
 
         while(!en_exploration.isEmpty()) {
-            closestNode = en_exploration.remove();
 
-            // Si le noeud a déjà été exploré
+            closestNode = en_exploration.remove();
+            int closestNodeId = closestNode.nodeId();
+
+            // Si le noeud a déjà été exploré, on l'ignore
             if (closestNode.distance() == Float.NEGATIVE_INFINITY) continue;
 
-            if (closestNode.nodeId() == endNodeId) {
-                return new SingleRoute(edges);
+            if (closestNodeId == endNodeId) {
+                int j = predecessor.length - 1;
+                // Construire la liste d'edges dans l'ordre de l'itinéraire
+                for (int i = predecessor.length - 1; i >= 0; i++) {
+                    for (Edge e : edges) {
+                        if (e.toNodeId() == predecessor[j]) {
+                            pathEdges.offerFirst(e);
+                            j = predecessor[j];
+                            break;
+                        }
+                    }
+                }
+                return new SingleRoute(new ArrayList<>(pathEdges));
             }
 
-            for (int i = 0; i < graph.nodeOutDegree(closestNode.nodeId()); i++) {
-                int i_thEdgeId = graph.nodeOutEdgeId(closestNode.nodeId(), i);
+            // Pour chaque arête sortant du closestNode
+            for (int i = 0; i < graph.nodeOutDegree(closestNodeId); i++) {
+                // Identité de l'arête sortante du plus proche noeud
+                int i_thEdgeId = graph.nodeOutEdgeId(closestNodeId, i);
+                // Noeud d'arrivée de l'arête
                 int edgeEndNodeId = graph.edgeTargetNodeId(i_thEdgeId);
                 // Facteur de coût -> allongement artificiel de la longueur d'une arête
-                double coeff = cityBikeCF.costFactor(closestNode.nodeId(), i_thEdgeId);
-                dist = distance[closestNode.nodeId()] + (float) (coeff * graph.edgeLength(i_thEdgeId));
+                double coeff = cityBikeCF.costFactor(closestNodeId, i_thEdgeId);
+                // Calcul distance
+                dist = distance[closestNodeId] + (float) (coeff * graph.edgeLength(i_thEdgeId));
 
                 if (dist < distance[edgeEndNodeId]) {
                     distance[edgeEndNodeId] = dist;
+                    predecessor[edgeEndNodeId] = closestNodeId;
                     en_exploration.add(new WeightedNode(edgeEndNodeId, dist));
-                    distance[closestNode.nodeId()] = Float.NEGATIVE_INFINITY;
-                    edges.add(Edge.of(graph, i_thEdgeId, closestNode.nodeId(), edgeEndNodeId));
+                    edges.set(edgeEndNodeId, Edge.of(graph, i_thEdgeId, closestNodeId, edgeEndNodeId));
                 }
             }
+            // Marquer le noeud exploré pour ne plus y revenir
+            distance[closestNodeId] = Float.NEGATIVE_INFINITY;
 
         }
         // Si aucun chemin n'a été trouvé
--- test/ch/epfl/javelo/routing/RouteComputerTest.java
+++ test/ch/epfl/javelo/routing/RouteComputerTest.java
@@ -1,6 +1,11 @@
 package ch.epfl.javelo.routing;
 
+import ch.epfl.javelo.KmlPrinter;
+import ch.epfl.javelo.data.Graph;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class RouteComputerTest {
 
