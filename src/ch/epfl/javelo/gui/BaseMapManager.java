e7459ebd6ea9496a9f0cb46281a7a0a0e1506585
Ngirod <nathanael.girod@epfl.ch>
2022-04-29 12:30:18 +0200

    Etape 8

--- src/ch/epfl/javelo/gui/BaseMapManager.java
+++ src/ch/epfl/javelo/gui/BaseMapManager.java
@@ -1,5 +1,6 @@
 package ch.epfl.javelo.gui;
 
+import ch.epfl.javelo.Math2;
 import javafx.application.Platform;
 import javafx.beans.property.ObjectProperty;
 import javafx.scene.canvas.Canvas;
@@ -29,7 +30,7 @@ public final class BaseMapManager {
         this.tileManager = tileManager;
         this.waypointsManager = waypointsManager;
         this.mapViewParametersP = mapViewParametersP;
-        this.canvas = new Canvas(300, 600);
+        this.canvas = new Canvas(600, 300);
         redrawOnNextPulse();
         canvas.sceneProperty().addListener((p, oldS, newS) -> {
             assert oldS == null;
@@ -37,6 +38,7 @@ public final class BaseMapManager {
         });
         pane = new Pane(this.canvas);
         canvas.widthProperty().bind(pane.widthProperty());
+        canvas.heightProperty().bind(pane.heightProperty());
     }
 
     /**
@@ -51,23 +53,26 @@ public final class BaseMapManager {
         if (!redrawNeeded) return;
         redrawNeeded = false;
         try {
-            Canvas newC = new Canvas(300, 600);
+            Canvas newC = new Canvas(600, 300);
             MapViewParameters mvp = mapViewParametersP.get();
             int zoomLevel = mvp.zoomLevel();
             double xImage = mvp.xImage();
             double yImage = mvp.yImage();
             final int TILE_SIZE = 256;
+            int xIndex =  Math2.ceilDiv((int)xImage , 256);
+            int yIndex =  Math2.ceilDiv((int)yImage , 256);
             for (int x = 0; x <= newC.getWidth(); x += TILE_SIZE) {
                 for (int y = 0; y <= newC.getHeight(); y += TILE_SIZE) {
-                    int xId = (int) scalb(xImage, -zoomLevel) + x;
-                    int yId = (int) scalb(yImage, -zoomLevel) + y;
-                    TileManager.TileId tileId = new TileManager.TileId(zoomLevel, xId, yId);
+                    System.out.println(xIndex + " " + yIndex);
+                    TileManager.TileId tileId = new TileManager.TileId(zoomLevel, xIndex++, yIndex++);
                     Image image = tileManager.imageForTileAt(tileId);
                     newC.getGraphicsContext2D()
                             .drawImage(image, x, y);
+                    System.out.println(x + " " + y );
                 }
                 canvas = newC;
-                pane = new Pane(canvas);
+                pane.getChildren().remove(canvas);
+                pane.getChildren().add(newC);
             }
         } catch (Exception ignored) {
             System.out.println(ignored);
