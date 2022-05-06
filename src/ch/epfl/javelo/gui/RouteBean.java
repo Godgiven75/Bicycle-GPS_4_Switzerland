1248fb862598c4dd5185b1caee2d02eedb59dfa6
GodGiven75 <tanguy.dieudonne02@gmail.com>
2022-05-06 10:14:55 +0200

    Etape 9

---
+++ src/ch/epfl/javelo/gui/RouteBean.java
@@ -0,0 +1,29 @@
+package ch.epfl.javelo.gui;
+
+import ch.epfl.javelo.routing.ElevationProfile;
+import ch.epfl.javelo.routing.Route;
+import ch.epfl.javelo.routing.RouteComputer;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.collections.ObservableList;
+
+/**
+ * Classe finale regroupant les propriétés relatives aux points de passage et à l'itinéraire
+ * correspondant.
+ *
+ * @author Tanguy Dieudonné (326618)
+ * @author Nathanaël Girod (329987)
+ */
+public final class RouteBean {
+    private RouteComputer routeComputer;
+    public ObservableList<Waypoint> waypoints;
+    public ReadOnlyObjectProperty<Route> route;
+    public DoubleProperty highlightedPosition; // la position mise en évidence
+    public ReadOnlyObjectProperty<ElevationProfile> elevationProfile;
+
+    public RouteBean(RouteComputer routeComputer) {
+        this.routeComputer = routeComputer;
+    }
+
+
+}
