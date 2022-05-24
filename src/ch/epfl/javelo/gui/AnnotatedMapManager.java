4039111008039f2e2ad95a9e4f16dc35efff1cb2
GodGiven75 <tanguy.dieudonne02@gmail.com>
2022-05-24 08:48:35 +0200

    Etape 11 Mardi 24/05 08:48

---
+++ src/ch/epfl/javelo/gui/AnnotatedMapManager.java
@@ -0,0 +1,13 @@
+package ch.epfl.javelo.gui;
+
+/**
+ * Gère l'affichage de la carte "annotée", càd le fond de la carte au-dessus
+ * duquel sont superposés l'itinéraire et les points de passage.
+ *
+ * @author Tanguy Dieudonné (326618)
+ * @author Nathanaël Girod (329987)
+ */
+public final class AnnotatedMapManager {
+
+
+}
