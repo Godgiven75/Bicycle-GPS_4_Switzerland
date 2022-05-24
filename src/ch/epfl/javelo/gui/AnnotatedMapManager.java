f74eb64f1a043bab7bd4906efb7a48128e99075d
Ngirod <nathanael.girod@epfl.ch>
2022-05-24 10:03:06 +0200

    24/05 10h02

--- src/ch/epfl/javelo/gui/AnnotatedMapManager.java
+++ src/ch/epfl/javelo/gui/AnnotatedMapManager.java
@@ -2,19 +2,23 @@ package ch.epfl.javelo.gui;
 
 import ch.epfl.javelo.data.Graph;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.ObservableList;
+import javafx.geometry.Point2D;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
-import java.util.ArrayList;
+
 import java.util.function.Consumer;
 
 /**
- * Gère l'affichage de la carte "annotée", càd le fond de la carte au-dessus
+ * Gère l'affichage de la carte "annotée", c.-à-d. le fond de la carte au-dessus
  * duquel sont superposés l'itinéraire et les points de passage.
  *
  * @author Tanguy Dieudonné (326618)
  * @author Nathanaël Girod (329987)
  */
+
 public final class AnnotatedMapManager {
     private final Graph graph;
     private final TileManager tileManager;
@@ -23,8 +27,19 @@ public final class AnnotatedMapManager {
     private final BaseMapManager baseMapManager;
     private final WaypointsManager waypointsManager;
     private final RouteManager routeManager;
+    private final StackPane pane;
+    private final SimpleDoubleProperty mousePositionOnRouteP;
+    private final ObjectProperty<Point2D> mousePositionP;
 
 
+
+    /**
+     * Construit un gestionnaire de carte "annotée".
+     * @param graph le graphe du réseau routier
+     * @param tileManager le gestionnaire de tuile
+     * @param routeBean le "bean" JavaFX correspondant à la route
+     * @param errorConsumer le consommateur d'erreur
+     */
     public AnnotatedMapManager(Graph graph, TileManager tileManager, RouteBean routeBean, Consumer<String> errorConsumer) {
         this.graph = graph;
         this.tileManager = tileManager;
@@ -38,8 +53,51 @@ public final class AnnotatedMapManager {
         baseMapManager = new BaseMapManager(tileManager, waypointsManager, mapViewParametersP);
 
         routeManager = new RouteManager(routeBean, mapViewParametersP);
+
+        pane = new StackPane();
+        pane.getStyleClass().add("map.css");
+
+        mousePositionOnRouteP = new SimpleDoubleProperty();
+        mousePositionP = new SimpleObjectProperty<>();
+        // Création de la hiérarchie JavaFX
+        pane.getChildren().add(baseMapManager.pane());
+        pane.getChildren().add(routeManager.pane());
+        pane.getChildren().add(waypointsManager.pane());
+
     }
 
+    /**
+     * Retourne le panneau principal du gestionnaire de carte "annotée".
+     * @return le panneau principal du gestionnaire de carte "annotée"
+     */
+    public Pane pane() {
+        return pane;
+    }
+
+    /**
+     * Retourne une propriété JavaFX contenant la position le long de l'itinéraire
+     * correspondant à la position de la souris.
+     * @return une propriété JavaFX contenant la position le long de l'itinéraire
+     * correspondant à la position de la souris
+     */
+    public SimpleDoubleProperty mousePositionOnRouteProperty() {
+        return mousePositionOnRouteP;
+    }
+
+    private void addMouseEventsManager() {
+        pane.setOnMouseEntered(e -> {
+            mousePositionP.set(new Point2D(e.getX(), e.getY()));
+        });
+
+        pane.setOnMouseExited(e -> {
+            mousePositionOnRouteP.set(Double.NaN);
+
+        });
+    }
+
+    private void addBindings() {
+
+    }
 
 
 }
