e1f91ca0648ba7195f4394ab394a51f534bdd6f0
GodGiven75 <tanguy.dieudonne02@gmail.com>
2022-05-27 14:13:37 +0200

    Etape 11 Vendredi 27/05 14:05

--- src/ch/epfl/javelo/gui/JaVelo.java
+++ src/ch/epfl/javelo/gui/JaVelo.java
@@ -3,7 +3,9 @@ package ch.epfl.javelo.gui;
 import ch.epfl.javelo.data.Graph;
 import ch.epfl.javelo.routing.*;
 import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
@@ -12,9 +14,10 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.Path;
 import java.util.function.Consumer;
 
@@ -30,8 +33,9 @@ public final class JaVelo extends Application {
     public static final int INITIAL_ZOOM_LEVEL = 12;
     public static final int INITIAL_X_IMAGE = 543200;
     public static final int INITIAL_Y_IMAGE = 370650;
+    public static final int MAX_STEP_LENGTH = 5;
     private BorderPane mainPane;
-    private SplitPane mapAndProfilePane;
+    private SplitPane mapAndProfilePane = new SplitPane();
     public JaVelo() {
         this.mainPane = new BorderPane();
     }
@@ -55,20 +59,22 @@ public final class JaVelo extends Application {
                 new SimpleObjectProperty<>(mapViewParameters);
         Consumer<String> errorConsumer = new ErrorConsumer();
 
-        RouteComputer routeComputer = new RouteComputer(graph, costFunction);
-
-        Route route = routeComputer
-                .bestRouteBetween()
+        RouteBean routeBean =
+                new RouteBean(new RouteComputer(graph, new CityBikeCF(graph)));
 
         ElevationProfile profile = ElevationProfileComputer
-                .elevationProfile(route, 5);
+                .elevationProfile(routeBean.route(), MAX_STEP_LENGTH);
 
         ObjectProperty<ElevationProfile> profileProperty =
                 new SimpleObjectProperty<>(profile);
 
-        RouteBean routeBean =
-                new RouteBean(new RouteComputer(graph, new CityBikeCF(graph)));
-        routeBean.setHighlightedPositionP(1000);
+        DoubleProperty highlightProperty = new SimpleDoubleProperty();
+
+        ElevationProfileManager elevationProfileManager =
+                new ElevationProfileManager(profileProperty,
+                        highlightProperty);
+        highlightProperty.bind(
+                elevationProfileManager.mousePositionOnProfileProperty());
 
         RouteManager routeManager =
                 new RouteManager(routeBean, mapViewParametersP);
@@ -79,15 +85,43 @@ public final class JaVelo extends Application {
                         routeBean.waypoints(),
                         errorConsumer);
 
+        BaseMapManager baseMapManager =
+                new BaseMapManager(tileManager,
+                        waypointsManager,
+                        mapViewParametersP);
+
+        AnnotatedMapManager annotatedMapManager =
+                new AnnotatedMapManager(graph,
+                        tileManager,
+                        routeBean,
+                        errorConsumer);
+
         mapAndProfilePane.setOrientation(Orientation.VERTICAL);
-        //mapAndProfilePane.getChildren
-        //StackPane stackPane = new StackPane()
+        // Lorsqu'un itinéraire existe
+        if (routeBean.route() != null) {
+            mapAndProfilePane
+                    .getChildrenUnmodifiable()
+                    .setAll(annotatedMapManager.pane(), elevationProfileManager.pane());
+            SplitPane.setResizableWithParent(elevationProfileManager.pane(), false);
+        }
+        else
+            mapAndProfilePane
+                    .getChildrenUnmodifiable()
+                    .setAll(annotatedMapManager.pane());
+
         mainPane.setCenter(mapAndProfilePane);
         MenuItem menuItem = new MenuItem("Exporter GPX");
         menuItem.disableProperty().bind(routeBean.routeProperty().isNull());
         Menu menu = new Menu("Fichier");
+        ElevationProfile elevationProfile =
+                ElevationProfileComputer.elevationProfile(routeBean.route(), 5f);
         menu.setOnAction(e -> {
-            GpxGenerator.createGpx(routeBean.route(), ElevationProfileManager.)
+            GpxGenerator.createGpx(routeBean.route(), elevationProfile);
+            try {
+                GpxGenerator.writeGpx("javelo.gpx", routeBean.route(), elevationProfile);
+            } catch (IOException ex) {
+                throw new UncheckedIOException(ex);
+            }
         });
         menu.getItems().add(menuItem);
         MenuBar menuBar = new MenuBar();
